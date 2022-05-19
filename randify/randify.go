6d64aabb530664fda90073353b0c1a97d97c3f48
drainuzzo <spremuta@gmail.com>
2022-05-19 18:59:23 +0200

    dir changes

--- randify/randify.go
+++ randify/randify.go
@@ -2,22 +2,8 @@ package randify
 
 import (
 	"math/rand"
-	"strings"
 	"time"
 )
 
-const charset = "abcdefghijklmnopqrstuvwxyz"
-
-const vowels = "aeiou"
-
 var SeededRand *rand.Rand = rand.New(
 	rand.NewSource(time.Now().UnixNano()))
-
-func Namify(n int) string {
-	sb := strings.Builder{}
-	sb.Grow(n)
-	for i := 0; i < n; i++ {
-		sb.WriteByte(charset[SeededRand.Intn(len(charset))])
-	}
-	return sb.String()
-}
