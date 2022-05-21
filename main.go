f64b3121fc12f20b4f315a151fde4e894f232ea9
Simone <simone.cibba@kyndryl.com>
2022-05-21 17:48:56 +0200

    random function changes, now works for vowels as well

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"strings"
 
 	"github.com/drainuzzo/gonamify/randify"
@@ -19,33 +18,42 @@ func main() {
 	// work in progress....
 
 	//fmt.Println("1:", rand.Int())
-	fmt.Println("2:", rand.Int())
-	fmt.Println("3:", rand.Int())
-	fmt.Println("4:", randify.SeededRand.Int())
+	//fmt.Println("2:", rand.Int())
+	//fmt.Println("3:", rand.Int())
+	//fmt.Println("4:", randify.SeededRand.Int())
 	var example string = "ciccciopelliccio"
 	for i, r := range example {
 		fmt.Println(i, r, string(r))
 	}
 	//rand.Seed(time.Now().UnixNano())
 	for i := 1; i <= 8; i++ {
-		fmt.Println("Name", i, " :", namify(10))
+
+		fmt.Println("Name", i, " :", rndString(10, charset))
+		fmt.Println("Name", i, " :", rndString(1, vowels))
 	}
 
 }
 
-func namify(n int) string {
+func rndString(n int, s string) string {
 	sb := strings.Builder{} //buffer
 	sb.Grow(n)
 	for i := 0; i < n; i++ {
-		sb.WriteByte(charset[randify.SeededRand.Intn(len(charset))])
+		sb.WriteByte(s[randify.SeededRand.Intn(len(s))])
 	}
 	return sb.String()
 }
 
+/* func fixCons(s *string) string {
+	for i := 0; i < n; i++ {
+
+	}
+}  */
+
 //TODO
-func rndVowel([]string) {
-	return //randomize vowels after 2? consonants
-}
+// func rndVowel(string) {
+
+// 	return //randomize vowels after 2? consonants
+// }
 
 // func rndCons([]string) {
 // 	return //randomize consonants
